c83cb4cba0d2ef711bf558fb7a9fdaf12896f097
RaphaelFreire <raphaelmachadofreire@gmail.com>
2016-05-15 15:01:40 -0300

    Este programa demonstra o membro de array length

---
+++ src/model/Length.java
@@ -0,0 +1,24 @@
+package model;
+
+/**
+ * @author raphaelmachadofreire
+ * Detalhe sobre métodos e classes
+ * Este programa demonstra o membro de array length
+ * Capítulo 7 - Página 147
+ */
+
+public class Length {
+    
+    public static void main(String args[]){
+        
+        int[] a1 = new int[10];
+        int[] a2 = {3,5,7,1,8,99,44,-10};
+        int[] a3 = {4,3,2,1};
+        
+        System.out.println("O tamamnho de a1 é " + a1.length);
+        System.out.println("O tamamnho de a2 é " + a2.length);
+        System.out.println("O tamamnho de a3 é " + a3.length);
+    
+    }
+    
+}
